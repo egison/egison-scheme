ad031794a1bef52c1c9a8265b173966b2f188a36
Satoshi Egi <egi@egison.org>
2018-12-21 21:57:43 +0900

    add multiset matcher

--- debug.scm
+++ debug.scm
@@ -4,31 +4,20 @@
 
 (extract-pattern-variables '(join _ (cons x (join _ (cons (val x) _)))))
 (extract-pattern-variables '(join _ (cons x (join _ (cons y _)))))
+(extract-pattern-variables `(cons x y))
 
 (gen-match-results 'x 'Something 10)
 
 (macroexpand '(match-all 10 Something [x x]))
 (macroexpand '(gen-match-results x Something 10))
+
 (match-all 10 Something ['x x])
-(match-all 10 Eq [`(val ,10) "OK"])
-(match-all 10 Integer [`(val ,10) "OK"])
+(match-all 10 Eq ['(val 10) "OK"])
+(match-all 10 Integer ['(val 10) "OK"])
 (match-all 10 Integer ['x x])
-(extract-pattern-variables `(cons x y))
 (match-all '(1 2 3) (List Integer) [`(cons x y) `(,x ,y)])
 (match-all '(1 2 3) (List Integer) [`(join x y) `(,x ,y)])
 (match-all '(1 2 3) (List Integer) [`(join _ (cons x _)) x])
 (match-all '(1 2 3) (List Integer) [`(join _ (cons x (join _ (cons y _)))) `(,x ,y)])
+(match-all '(1 2 3) (Multiset Integer) [`(cons x (cons y _)) `(,x ,y)])
 (match-all '(1 2 3 1) (List Integer) [`(join _ (cons x (join _ (cons (val x) _)))) `(,x ,y)])
-
-(macroexpand '(match-all {1 2 3 1} (List integer)
-                         [(join _ (cons x (join _ (cons y _))))
-                          `(,x ,y)]))
-
-
-(match-all {1 2 3 1} (List integer)
-                         [(join _ (cons x (join _ (cons y _))))
-                          `(,x ,y)])
-
-(macroexpand '(match-all {1 2 3 1} (List integer)
-                         [(join _ (cons x (join _ (cons y _))))
-                          `(,x ,y)]))
--- egison.scm
+++ egison.scm
@@ -1,11 +1,11 @@
 (use util.match)
 (use srfi-1)
 
-(define-macro (match-all t m . clauses)
+(define-macro (match-all t M . clauses)
   (let* {[clause (car clauses)]
          [p (car clause)]
          [e (cadr clause)]}
-    `(map (lambda (ret) (apply (lambda ,(extract-pattern-variables p) ,e) ret)) (gen-match-results ,p ,m ,t))))
+    `(map (lambda (ret) (apply (lambda ,(extract-pattern-variables p) ,e) ret)) (gen-match-results ,p ,M ,t))))
 
 (define extract-pattern-variables
   (lambda (p)
@@ -17,8 +17,8 @@
            (pvar `(,pvar))
            )))
 
-(define-macro (gen-match-results p m t)
-  `(processMStates (list (list 'MState (list (list ,p ,m ,t) ) {}))))
+(define-macro (gen-match-results p M t)
+  `(processMStates (list (list 'MState (list (list ,p ,M ,t) ) {}))))
 
 (define processMStates
   (lambda (mStates)
@@ -32,8 +32,8 @@
             (processMStates (cons `(MState ,mStack ,ret) rs)))
            ((('MState {[pvar 'Something t] . mStack} ret) . rs)
             (processMStates (cons `(MState ,mStack ,(append ret `(,t))) rs)))
-           ((('MState {[p m t] . mStack} ret) . rs)
-            (let {[next-matomss (m p t)]}
+           ((('MState {[p M t] . mStack} ret) . rs)
+            (let {[next-matomss (M p t)]}
               (processMStates (append (map (lambda (next-matoms) `(MState ,(append next-matoms mStack) ,ret)) next-matomss) rs))))
            )))
 
@@ -85,4 +85,14 @@
             (cons `(() ,xs) (map (lambda (p) `(,(cons y (car p)) ,(cadr p))) (unjoin ys))))
            )))
 
-(unjoin '(1 2 3))
+(define Multiset
+  (lambda (M)
+    (lambda (p t)
+      (match p
+             (('cons px py)
+              (map (lambda (xy) `((,px ,M ,(car xy)) (,py ,(Multiset M) ,(cadr xy))))
+                   (match-all t (List M)
+                              ['(join hs (cons x ts)) `(,x ,(append hs ts))])))
+             (pvar
+              `(((,pvar Something ,t))))
+             ))))
