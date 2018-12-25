843fb2e7a080ca11b9a25e0a7b266d9df0eeb6ff
Satoshi Egi <egi@egison.org>
2018-12-25 10:36:52 +0900

    add pattern-matching-oriented-programming-style.egi

--- debug.scm
+++ debug.scm
@@ -35,23 +35,3 @@
               })
 
 
-(define pm-map
-  (lambda (f xs)
-    (match-all xs (List Something)
-               (`(join _ (cons x _)) (f x)))))
-
-(pm-map (lambda (x) (+ x 10)) `(1 2 3 4))
-
-(define pm-concat
-  (lambda (xss)
-    (match-all xss (List (List Something))
-               (`(join _ (cons (join _ (cons x _)) _)) x))))
-
-(pm-concat `((1 2) (3) (4 5)))
-
-(define pm-concat2
-  (lambda (xss)
-    (match-all xss (Multiset (Multiset Something))
-               (`(cons (cons x _)) x))))
-
-(pm-concat2 `((1 2) (3) (4 5)))
